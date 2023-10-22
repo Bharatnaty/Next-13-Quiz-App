fd3af591c22ccb445d4c358c52166eda885ad444
unknown <bharatnatrayn00@gmail.com>
2023-10-22 18:40:51 +0530

    Quiz App

---
+++ app/api/quiz/[id]/route.js
@@ -0,0 +1,24 @@
+/**
+ * Retrieves a question and generates a random question for the quiz.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} params - The route parameters.
+ * @param {string} params.id - The ID of the question to retrieve.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the question and a randomly generated question, or an error response.
+ */
+
+import questions from '@/data/quiz.json'
+import { NextResponse } from 'next/server'
+
+export async function GET(req, { params }) {
+  try {
+    const question = questions.data.find(item => item.id === params.id)
+    const { correct_answer, ...rest } = question
+    return NextResponse.json({
+      question: rest,
+    })
+  } catch (error) {
+    return new NextResponse('Internal server error', { status: 500 })
+  }
+}
---
+++ app/api/quiz/random/route.js
@@ -0,0 +1,22 @@
+/**
+ * Retrieves a random question from the quiz.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the ID of a randomly selected question, or an error response.
+ */
+
+import questions from '@/data/quiz.json'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try{
+    const random = Math.floor(Math.random() * questions.data.length )
+    return NextResponse.json({
+      randomQuestion: questions.data[random].id
+    })
+  }
+  catch(error)
+  {
+    console.log(error)
+    return new NextResponse.json("Internal server error",{status:500})
+  }
+}
